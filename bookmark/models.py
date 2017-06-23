547b2a01d9f4b0bb283ff581a589e96f6e5b3185
Junshoong <junshoong@gmail.com>
2017-06-24 02:26:43 +0900

    add Bookmark model

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.URLField()
+    writer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s %s' % (self.title, self.link)
+
+
+
--- bookmark/tests.py
+++ bookmark/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from bookmark.models import Bookmark, Tag
 
-# Create your tests here.
+
+class BookmarkModelTest(TestCase):
+
+    @staticmethod
+    def _create_user():
+        user = get_user_model().objects.create(username='test')
+        user.set_password('test')
+        user.save()
+        return user
+
+    def test_create_Bookmark(self):
+        user = self._create_user()
+        first_bookmark = Bookmark()
+        first_bookmark.title = '구글'
+        first_bookmark.link = 'https://www.google.co.kr'
+        first_bookmark.writer = user
+        first_bookmark.save()
+        first_tag = Tag(name='portal')
+        first_tag.save()
+        second_tag = Tag(name='search')
+        second_tag.save()
+        first_bookmark.tag.add(first_tag, second_tag)
+
+        saved_bookmark = Bookmark.objects.all()
+        self.assertEqual(saved_bookmark.count(), 1)
+        self.assertEqual(saved_bookmark[0].tag.all()[0], first_tag)
+        self.assertEqual(saved_bookmark[0].tag.all()[1], second_tag)
+        self.assertEqual(saved_bookmark[0].title, '구글')
+
+
+
