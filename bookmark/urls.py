1f92e9b50d0ef691e3660979e152ce066da9c3b2
Junshoong <junshoong@gmail.com>
2017-06-26 17:18:00 +0900

    Modify bookmark view from CBV to FBV

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from bookmark.views import BookmarkLV
+from bookmark.views import list_and_create
 
 urlpatterns = [
-    url(r'^$', BookmarkLV.as_view(), name='bookmark_list'),
+    url(r'^$', list_and_create, name='bookmark_list'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,14 +1,17 @@
-from django.views.generic import ListView
 from django.contrib.auth.models import AnonymousUser
+from django.shortcuts import render
 from bookmark.models import Bookmark
 
 
-class BookmarkLV(ListView):
-    model = Bookmark
-    template_name = 'bookmark/bookmark_list.html'
-    context_object_name = 'bookmark'
+def list_and_create(request):
+    if request.method == 'POST':
+        pass
 
-    def get_queryset(self):
-        if isinstance(self.request.user, AnonymousUser):
-            return
-        return Bookmark.objects.filter(writer=self.request.user)
+    if isinstance(request.user, AnonymousUser):
+        objects = None
+    else:
+        objects = Bookmark.objects.filter(writer=request.user)
+    return render(request, 'bookmark/bookmark_list.html', {
+        'bookmark': objects,
+    })
+
